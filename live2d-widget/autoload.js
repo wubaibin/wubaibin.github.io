a94fcf66ba0af05618ce1ca4be23a0e5eaf5333d
wubaibin <2448428125@qq.com>
2023-01-13 16:20:24 +0800

    Site updated: 2023-01-13 16:20:24

--- live2d-widget/autoload.js
+++ live2d-widget/autoload.js
@@ -1,5 +1,5 @@
 // 注意：live2d_path 参数应使用绝对路径
-const live2d_path = "https://haiyong.site/live2d-widget/";
+const live2d_path = "https://fastly.jsdelivr.net/gh/stevenjoezhang/live2d-widget@latest/";
 //const live2d_path = "/live2d-widget/";
 
 // 封装异步加载资源的方法
@@ -33,7 +33,7 @@ if (screen.width >= 768) {
 	]).then(() => {
 		initWidget({
 			waifuPath: live2d_path + "waifu-tips.json",
-			//apiPath: "https://live2d.fghrsh.net/api/",
+			apiPath: "https://live2d.fghrsh.net/api/",
 			cdnPath: "https://npm.elemecdn.com/akilar-live2dapi@latest/"
 		});
 	});
